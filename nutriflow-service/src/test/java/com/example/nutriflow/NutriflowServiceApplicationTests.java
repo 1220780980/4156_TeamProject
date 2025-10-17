aba733c3583d3b74b29367acbb7b9520863f4a1a
Erin Zhang <erinzhang0215@gmail.com>
2025-10-17 14:13:48 -0400

    initial project structure setup

---
+++ nutriflow-service/src/test/java/com/example/nutriflow/NutriflowServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package test.java.com.example.nutriflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+        
+    }
+
+}
