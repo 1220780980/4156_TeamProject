4d5a9dd84c7732fab450a8ad1f747e7509650f9d
Erin Zhang <erinzhang0215@gmail.com>
2025-12-01 21:09:01 -0500

    small fix for fitness backend

--- fitness-client/backend/src/main/java/com/example/fitness/controller/AppUserController.java
+++ fitness-client/backend/src/main/java/com/example/fitness/controller/AppUserController.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/users")
+@CrossOrigin(origins = "http://localhost:3000")
 public class AppUserController {
 
     /**
